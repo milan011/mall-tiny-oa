43639f2be0096449426bf004e851186cb6b0a0cd
wcgInHome <milan011@sohu.com>
2023-02-05 19:30:27 +0800

    部门状态

--- src/main/java/com/macro/mall/tiny/modules/ums/controller/UmsDepartmentController.java
+++ src/main/java/com/macro/mall/tiny/modules/ums/controller/UmsDepartmentController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.macro.mall.tiny.common.api.CommonPage;
 import com.macro.mall.tiny.common.api.CommonResult;
 import com.macro.mall.tiny.modules.ums.model.UmsDepartment;
+import com.macro.mall.tiny.modules.ums.model.UmsRole;
 import com.macro.mall.tiny.modules.ums.service.UmsDepartmentService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,6 +66,20 @@ public class UmsDepartmentController {
   @ResponseBody
   public CommonResult delete(@RequestParam("ids") List<Long> ids) {
     boolean success = departmentService.delete(ids);
+    if (success) {
+      return CommonResult.success(null);
+    }
+    return CommonResult.failed();
+  }
+
+  @ApiOperation("修改部门状态")
+  @RequestMapping(value = "/updateStatus/{id}", method = RequestMethod.POST)
+  @ResponseBody
+  public CommonResult updateStatus(@PathVariable Long id, @RequestParam(value = "status") Integer status) {
+    UmsDepartment department = new UmsDepartment();
+    department.setId(id);
+    department.setStatus(status);
+    boolean success = departmentService.updateById(department);
     if (success) {
       return CommonResult.success(null);
     }
