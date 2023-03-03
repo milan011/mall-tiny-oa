fd42e7f1115d80f2e673a8c4071ce6f239b42481
wcgInHome <milan011@sohu.com>
2023-03-03 09:18:25 +0800

    OSS图片上传

---
+++ src/main/java/com/macro/mall/tiny/modules/ums/controller/OssController.java
@@ -0,0 +1,45 @@
+package com.macro.mall.tiny.modules.ums.controller;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.modules.ums.dto.OssCallbackResult;
+import com.macro.mall.tiny.modules.ums.dto.OssPolicyResult;
+import com.macro.mall.tiny.modules.ums.service.OssService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Oss对象存储管理Controller
+ * Created by macro on 2018/4/26.
+ */
+@RestController
+@Api(tags = "UmsAdminController")
+@Tag(name = "UmsAdminController",description = "后台用户管理")
+@RequestMapping("/aliyun/oss")
+public class OssController {
+    @Autowired
+    private OssService ossService;
+
+    @ApiOperation(value = "Oss上传签名生成")
+    @RequestMapping(value = "/policy", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<OssPolicyResult> policy() {
+        OssPolicyResult result = ossService.policy();
+        return CommonResult.success(result);
+    }
+
+    @ApiOperation(value = "Oss上传成功回调")
+    @RequestMapping(value = "callback", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<OssCallbackResult> callback(HttpServletRequest request) {
+        OssCallbackResult ossCallbackResult = ossService.callback(request);
+        return CommonResult.success(ossCallbackResult);
+    }
+}
